490e3e836ed720830f78f3d73dded70a14b3a410
AlejandroPera <A01377806@itesm.mx>
2023-02-02 22:07:36 -0600

    Animaciones on scroll y background svg animado

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
-window.onwheel = e => {
-    if(e.deltaY >= 0){
-      // Scrolling Down with mouse
-      console.log('Scroll Down');
+// window.onwheel = e => {
+//     if(e.deltaY >= 0){
+//       // Scrolling Down with mouse
+//       console.log('Scroll Down');
+//     } else {
+//       // Scrolling Up with mouse
+//       console.log('Scroll Up');
+//     }
+//   }
+
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
     } else {
-      // Scrolling Up with mouse
-      console.log('Scroll Up');
+      reveals[i].classList.remove("active");
     }
   }
+}
 
+window.addEventListener("scroll", reveal);
