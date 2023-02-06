3991a687f086087122015c46175930b689c047b4
AlejandroPera <A01377806@itesm.mx>
2023-02-06 17:45:24 -0600

    some responsive features added

--- app.js
+++ app.js
@@ -9,7 +9,35 @@
 //   }
 
 
+// var bg = document.getElementsByClassName("background-1")[0];
+//   bg.style.backgroundSize = "60px 120px"
+
+
+// const width = window.innerWidth;
+// const height = window.innerHeight;
+// var bg_1 = document.getElementsByClassName("background-1")[0];
+// var bg_2 = document.getElementsByClassName("background-2")[0];
+// if (width/height <= 1.5){
+//   bg_1.classList.add("small-ratio")
+//   bg_2.classList.add("small-ratio")
+// }
+
+let root = document.documentElement;
+
+function fitContent(){
+  const ratio = window.innerWidth/window.innerHeight;
+var bg_1 = document.getElementsByClassName("background-1")[0];
+var bg_2 = document.getElementsByClassName("background-2")[0];
+  if (ratio < 1.5){
+    bg_1.classList.add("small-ratio")
+    bg_2.classList.add("small-ratio")
+    const adjustmentCurve = 1.448+0.131*ratio+0.008*(ratio**2)+0.00016*(ratio**3)
+    root.style.setProperty('--bg-translate', adjustmentCurve+"em");
+  }
+}
+
 function reveal() {
+  
   var reveals = document.querySelectorAll(".reveal");
   for (var i = 0; i < reveals.length; i++) {
     var windowHeight = window.innerHeight;
@@ -23,4 +51,28 @@ function reveal() {
   }
 }
 
+function responsive(event){
+  const ratio = window.innerWidth/window.innerHeight;
+  var bg_1 = document.getElementsByClassName("background-1")[0];
+  var bg_2 = document.getElementsByClassName("background-2")[0];
+  
+  if (ratio <= 1.5){
+    bg_1.classList.add("small-ratio")
+    bg_2.classList.add("small-ratio")
+    const adjustmentCurve = (ratio-3.1)**5*(0.1)**ratio
+    alert(ratio)
+    alert(adjustmentCurve)
+    root.style.setProperty('--bg-translate', adjustmentCurve+"em");
+  }
+  else{
+    root.style.setProperty('--bg-translate', 0+"em")
+    bg_1.classList.remove("small-ratio")
+    bg_2.classList.remove("small-ratio")
+  }
+}
+window.addEventListener("DOMContentLoaded", fitContent);
 window.addEventListener("scroll", reveal);
+window.addEventListener("resize", responsive);
+
+// var bg = document.getElementsByClassName("background-1")[0];
+//   bg.style.backgroundSize = "60px 120px"
