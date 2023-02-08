210f342ee97b2cb902c31c9a4f3c949598608ec7
AlejandroPera <A01377806@itesm.mx>
2023-02-07 21:16:32 -0600

    SVG Animation upon scroll

--- app.js
+++ app.js
@@ -26,28 +26,37 @@ let root = document.documentElement;
 
 function fitContent(){
   const ratio = window.innerWidth/window.innerHeight;
-var bg_1 = document.getElementsByClassName("background-1")[0];
-var bg_2 = document.getElementsByClassName("background-2")[0];
-  if (ratio < 1.5){
-    bg_1.classList.add("small-ratio")
-    bg_2.classList.add("small-ratio")
-    const adjustmentCurve = 1.448+0.131*ratio+0.008*(ratio**2)+0.00016*(ratio**3)
-    root.style.setProperty('--bg-translate', adjustmentCurve+"em");
-  }
+  var bg_1 = document.getElementsByClassName("background-1")[0];
+  var bg_2 = document.getElementsByClassName("background-2")[0];
+    if (ratio < 1.5){
+      bg_1.classList.add("small-ratio")
+      bg_2.classList.add("small-ratio")
+      const adjustmentCurve = (ratio - 1.50308515)/0.03738281
+      root.style.setProperty('--bg-translate', adjustmentCurve+"em");
+    }
 }
 
 function reveal() {
   
-  var reveals = document.querySelectorAll(".reveal");
+  const reveals = document.querySelectorAll(".reveal");
   for (var i = 0; i < reveals.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 150;
+    const windowHeight = window.innerHeight;
+    const elementTop = reveals[i].getBoundingClientRect().top;
+    const elementBottom = reveals[0].getBoundingClientRect().bottom;
+    const elementVisible = 40;
+    const SVG = document.querySelectorAll(".SVG")[0];
     if (elementTop < windowHeight - elementVisible) {
+      if (reveals[i].className.includes("transition") && !SVG.getAttribute("data")){
+        SVG.setAttribute("data", "./images/bg_animated.svg")
+      }
       reveals[i].classList.add("active");
     } else {
+      if (reveals[i].className.includes("transition") && SVG.getAttribute("data")){
+        SVG.setAttribute("data", "")
+      }
       reveals[i].classList.remove("active");
     }
+    // else if(elementBottom) < windowHeight - elementVisible)
   }
 }
 
@@ -59,9 +68,8 @@ function responsive(event){
   if (ratio <= 1.5){
     bg_1.classList.add("small-ratio")
     bg_2.classList.add("small-ratio")
-    const adjustmentCurve = (ratio-3.1)**5*(0.1)**ratio
-    alert(ratio)
-    alert(adjustmentCurve)
+    const adjustmentCurve = (ratio - 1.50308515)/0.03738281
+    // alert(ratio)
     root.style.setProperty('--bg-translate', adjustmentCurve+"em");
   }
   else{
@@ -70,6 +78,8 @@ function responsive(event){
     bg_2.classList.remove("small-ratio")
   }
 }
+
+
 window.addEventListener("DOMContentLoaded", fitContent);
 window.addEventListener("scroll", reveal);
 window.addEventListener("resize", responsive);
